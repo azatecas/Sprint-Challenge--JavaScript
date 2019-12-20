a12c83870686f762391101a32b53bc3c0030c4da
Luis PM <luisp237@gmail.com>
2019-12-20 13:18:37 -0500

    mvp

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -92,9 +92,14 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach((item) => {
+  if(item.university.toLowerCase().includes('uni')){
+    return unisWithUni.push(item.university);
+  }
+})
 
 
-console.log();
+console.log(unisWithUni);
 
 
 // ==== ADVANCED Array Methods ====
